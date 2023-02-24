e56eb2be0c5444353716823a8ce8b5cf1757a9b7
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-02-24 03:41:15 +0000

    UserId, AppUser, UserSession and improved Persistence

---
+++ example/lib/persistence.dart
@@ -0,0 +1,69 @@
+import 'package:oauth/flow.dart';
+import 'package:oauth/oauth.dart';
+
+// https://github.com/simolus3/drift/branches
+// https://drift.simonbinder.eu/docs/advanced-features/
+// https://pub.dev/packages/stormberry
+class InMemoryPersistance extends Persistence {
+  final Map<String, AuthStateModel> mapState = {};
+  final Map<String, AuthUser> mapAuthUser = {};
+  final Map<String, AppUser> mapAppUser = {};
+  final Map<String, UserSession> mapSession = {};
+
+  @override
+  Future<AuthStateModel?> getState(String key) async {
+    return mapState[key];
+  }
+
+  @override
+  Future<void> setState(String key, AuthStateModel value) async {
+    mapState[key] = value;
+  }
+
+  @override
+  Future<List<AppUser?>> getUsersById(List<UserId> ids) async {
+    return ids.map((id) {
+      if (id.kind == UserIdKind.innerId) {
+        return mapAppUser[id.id];
+      }
+      // return mapAuthUser.values.cast<AuthUser?>().firstWhere(
+      //       (user) => user!.userIds().contains(id),
+      //       orElse: () => null,
+      //     );
+      return mapAppUser.values.cast<AppUser?>().firstWhere(
+            (user) => user!.userIds().contains(id),
+            orElse: () => null,
+          );
+    }).toList();
+  }
+
+  @override
+  Future<UserSession?> getAnySession(String sessionId) async {
+    return mapSession[sessionId];
+  }
+
+  @override
+  Future<void> saveSession(UserSession session) async {
+    mapSession[session.sessionId] = session;
+  }
+
+  @override
+  Future<void> saveUser(String userId, AuthUser user) async {
+    mapAuthUser[user.key] = user;
+    final prevUser = mapAppUser[userId];
+    if (prevUser == null) {
+      // create new user
+      mapAppUser[userId] = AppUser.merge(userId, [user]);
+    } else {
+      // update authUsers
+      final index = prevUser.authUsers.indexWhere((u) => u.key == user.key);
+      if (index == -1) {
+        prevUser.authUsers.add(user);
+      } else {
+        prevUser.authUsers[index] = user;
+      }
+      // add info from new provider
+      mapAppUser[userId] = AppUser.merge(userId, prevUser.authUsers);
+    }
+  }
+}
--- lib/src/data.dart
+++ lib/src/data.dart
@@ -1,20 +1,129 @@
-import 'dart:convert';
-
+import 'package:meta/meta.dart';
+import 'package:oauth/flow.dart';
 import 'package:oauth/oauth.dart';
 import 'package:oauth/providers.dart';
 
 abstract class Persistence {
-  Future<String?> getState(String key);
-  Future<void> setState(String key, String value);
+  /// Retrieves a saved [AuthStateModel] associated with [key],
+  /// persisted using [setState].
+  Future<AuthStateModel?> getState(String key);
 
-  Future<void> saveUser(AuthUser user) {
-    return setState(
-      // TODO: provider.name may be "other" maybe use a String instead of enum
-      '${user.userAppId}:${user.provider.name}',
-      jsonEncode(user.toJson()),
+  /// Saves a [value] and associates it with [key].
+  Future<void> setState(String key, AuthStateModel value);
+
+  /// Saves a [session] when the user has signed in or registered.
+  Future<void> saveSession(UserSession session);
+
+  /// Returns a any (valid or not valid) session with [sessionId].
+  Future<UserSession?> getAnySession(String sessionId);
+
+  /// Returns a valid session with [sessionId].
+  Future<UserSession?> getValidSession(String sessionId) =>
+      getAnySession(sessionId)
+          .then((value) => value?.endedAt == null ? value : null);
+
+  /// Saves a [user] and associates it with a [userId]
+  Future<void> saveUser(String userId, AuthUser user);
+
+  /// Retrieves users by ids. The array should have the same length.
+  Future<List<AppUser?>> getUsersById(List<UserId> ids);
+}
+
+@immutable
+class UserId {
+  final String id;
+  final UserIdKind kind;
+
+  const UserId(this.id, this.kind);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserId && id == other.id && kind == other.kind;
+
+  @override
+  int get hashCode => Object.hash(id, kind);
+}
+
+enum UserIdKind {
+  /// Global app id
+  innerId,
+
+  /// User id for the given provider
+  providerId,
+
+  /// Verifier email address
+  verifiedEmail,
+
+  /// Verifier phone number
+  verifiedPhone,
+}
+
+/// A user session, persisted when the user is signed in.
+class UserSession {
+  final String sessionId;
+  final String refreshToken;
+  final String userId;
+  final DateTime createdAt;
+  final String? deviceId;
+  final Map<String, Object?>? meta;
+  final DateTime? endedAt;
+
+  /// A user session, persisted when the user is signed in.
+  const UserSession({
+    required this.sessionId,
+    required this.refreshToken,
+    required this.userId,
+    required this.createdAt,
+    this.deviceId,
+    this.meta,
+    this.endedAt,
+  });
+// generated-dart-fixer-start{"md5Hash":"UR44W1AtB7vHXyyYToVCGQ=="}
+
+  factory UserSession.fromJson(Map json) {
+    return UserSession(
+      deviceId: json['deviceId'] as String?,
+      sessionId: json['sessionId'] as String,
+      refreshToken: json['refreshToken'] as String,
+      userId: json['userId'] as String,
+      meta: json['meta'] == null
+          ? null
+          : (json['meta'] as Map).map((k, v) => MapEntry(k as String, v)),
+      endedAt: json['endedAt'] == null
+          ? null
+          : DateTime.parse(json['endedAt'] as String),
+      createdAt: DateTime.parse(json['createdAt'] as String),
     );
   }
+
+  Map<String, Object?> toJson() {
+    return {
+      'deviceId': deviceId,
+      'sessionId': sessionId,
+      'refreshToken': refreshToken,
+      'userId': userId,
+      'meta': meta,
+      'endedAt': endedAt?.toIso8601String(),
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  @override
+  String toString() {
+    return "UserSession${{
+      "deviceId": deviceId,
+      "sessionId": sessionId,
+      "refreshToken": refreshToken,
+      "userId": userId,
+      "meta": meta,
+      "endedAt": endedAt,
+      "createdAt": createdAt,
+    }}";
+  }
 }
+
+// generated-dart-fixer-end{"md5Hash":"UR44W1AtB7vHXyyYToVCGQ=="}
 
 T matchProvider<T>(
   OAuthProvider provider, {
@@ -27,9 +136,10 @@ T matchProvider<T>(
   // T Function(linkedinProvider)? linkedin,
   T Function(MicrosoftProvider)? microsoft,
   T Function(RedditProvider)? reddit,
-  // T Function(steamProvider)? steam,
-  T Function(TwitterProvider)? twitter,
   T Function(SpotifyProvider)? spotify,
+  // T Function(SteamProvider)? steam,
+  T Function(TwitchProvider)? twitch,
+  T Function(TwitterProvider)? twitter,
 }) {
   if (provider is AppleProvider) {
     return (apple ?? other)(provider);
@@ -47,14 +157,74 @@ T matchProvider<T>(
     return (microsoft ?? other)(provider);
   } else if (provider is RedditProvider) {
     return (reddit ?? other)(provider);
-    // } else if (provider is steamProvider) {
-    // return (steam ?? other)(provider);
-  } else if (provider is TwitterProvider) {
-    return (twitter ?? other)(provider);
   } else if (provider is SpotifyProvider) {
     return (spotify ?? other)(provider);
   }
+  //  else if (provider is SteamProvider) {
+  // return (steam ?? other)(provider);
+  else if (provider is TwitchProvider) {
+    return (twitch ?? other)(provider);
+  } else if (provider is TwitterProvider) {
+    return (twitter ?? other)(provider);
+  }
   return other(provider);
+}
+
+class AppUser {
+  final String userId;
+  final String? name;
+  final String? profilePicture;
+  final String? email;
+  final bool emailIsVerified;
+  final String? phone;
+  final bool phoneIsVerified;
+  final List<AuthUser<dynamic>> authUsers;
+
+  ///
+  const AppUser({
+    required this.userId,
+    this.name,
+    this.profilePicture,
+    this.email,
+    required this.emailIsVerified,
+    this.phone,
+    required this.phoneIsVerified,
+    required this.authUsers,
+  });
+
+  // TODO: merge with base AppUser
+  factory AppUser.merge(String userId, List<AuthUser<dynamic>> authUsers) {
+    final list = authUsers.cast<AuthUser<dynamic>?>();
+    final email = list.firstWhere(
+      (auth) => auth!.emailIsVerified && auth.email != null,
+      orElse: () => null,
+    );
+    final phone = list.firstWhere(
+      (auth) => auth!.phoneIsVerified && auth.phone != null,
+      orElse: () => null,
+    );
+    final name = list.firstWhere(
+      (auth) => auth!.name != null,
+      orElse: () => null,
+    );
+    final profilePicture = list.firstWhere(
+      (auth) => auth!.profilePicture != null,
+      orElse: () => null,
+    );
+    return AppUser(
+      userId: userId,
+      emailIsVerified: email != null,
+      phoneIsVerified: phone != null,
+      email: email?.email,
+      phone: phone?.phone,
+      name: name?.name,
+      profilePicture: profilePicture?.profilePicture,
+      authUsers: authUsers,
+    );
+  }
+
+  Iterable<UserId> userIds() => [UserId(userId, UserIdKind.innerId)]
+      .followedBy(authUsers.expand((auth) => auth.userIds()));
 }
 
 class AuthUser<T> {
@@ -84,6 +254,19 @@ class AuthUser<T> {
   final Map<String, Object?> rawUserData;
   final OpenIdClaims? openIdClaims;
   final T providerUser;
+
+  /// A global unique key for the user
+  String get key => '$providerId:$providerUserId';
+
+  List<UserId> userIds() {
+    return [
+      UserId(key, UserIdKind.providerId),
+      if (emailIsVerified && email != null)
+        UserId(email!, UserIdKind.verifiedEmail),
+      if (phoneIsVerified && phone != null)
+        UserId(phone!, UserIdKind.verifiedPhone),
+    ];
+  }
 
   static AuthUser<OpenIdClaims> fromClaims(
     OpenIdClaims claims, {
